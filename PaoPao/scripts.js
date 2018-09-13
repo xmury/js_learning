9002955936faf4ab74888f39a5fb1dc89ef2a94c
XmuryNik <xmury.ya@gmail.com>
2018-09-13 13:47:45 +0600

    genArea update

--- PaoPao/scripts.js
+++ PaoPao/scripts.js
@@ -1,23 +1,29 @@
-var area = [];
-var arr = ['red'];
+var area = [[]];
+var arr = ['red', 'blue', 'green' , 'white' , 'brown' , 'gray' , 'orange' , ""];
 function genArea(){
     var xMax = document.getElementById('x').value;
     var yMax = document.getElementById('y').value;
-    // console.log(x + "|" + y);
+    var size = 100/xMax;
+    console.log(area);
     document.write("<div class='area'>");
-    for (var y = 1; y <= yMax; y++ ){
+    var x = 0; var y = 0;
+    for (; y <= yMax; y++ ){
+        area[x] = [];
+        // console.log(area[x]);
         document.write("<div='column'>");
-        for (var x = 1; x <= xMax; x++ ){
-            var min = 0; var max = arr.lenht
-			var rand = min - 0.5 + Math.random() * (max - min + 1); rand = Math.round(rand);            
-            area[x][y] = rand;
+        for (; x <= xMax; x++ ){
+            var min = 0; var max = arr.length;
+            var rand = min - 0.5 + Math.random() * (max - min + 1); rand = Math.round(rand);            
+            // console.log(area + " | " + rand);
+            area[x] = rand;
             document.write("<div='row'>");
-            document.write('<button class="btn" onclick="click()"> ' + val + ' </button>');
+            document.write(`<button class='btn' onclick='click()' style=' width:${size}%; height:${size}%; background:${arr[rand]};'> </button>`);
             document.write("</div>");
         }
+        x = 0;
         document.write("</div>");
     }
-
+    document.write("</div>");
 }
 
 function click(){
