f8d204cc26af0e40a9f03d5453dfa5bd7e561d56
xmury <xmury.ya@gmail.com>
2018-09-13 22:22:23 +0500

    genArea up

--- PaoPao/scripts.js
+++ PaoPao/scripts.js
@@ -3,8 +3,9 @@ var arr = ['red', 'blue', 'green' , 'white' , 'brown' , 'gray' , 'orange' , ""];
 function genArea(){
     var xMax = document.getElementById('x').value;
     var yMax = document.getElementById('y').value;
-    var size = 100/xMax;
+    var size = '60px';
     console.log(area);
+    document.write(`<style> .area { width: 800px; height: 600px; } </style>`)
     document.write("<div class='area'>");
     var x = 0; var y = 0;
     for (; y <= yMax; y++ ){
@@ -16,13 +17,12 @@ function genArea(){
             var rand = min - 0.5 + Math.random() * (max - min + 1); rand = Math.round(rand);            
             // console.log(area + " | " + rand);
             area[x] = rand;
-            document.write("<div='row'>");
-            document.write(`<button class='btn' onclick='click()' style=' width:${size}%; height:${size}%; background:${arr[rand]};'> </button>`);
-            document.write("</div>");
+            document.write("<div='box'>");
+            document.write(`<button class='btn' onclick='click()' style=' width:${size}; height:${size}; background:${arr[rand]};'> </button>`);
         }
         x = 0;
-        document.write("</div>");
     }
+
     document.write("</div>");
 }
 
